ed1db8ece2e73d119452fc6b2cfc968f311071cc
GeoAvila <geovanny.avila@gmail.com>
2013-05-31 00:06:26 -0600

    Commit Day 3

--- Ejercicio3/Program.cs
+++ Ejercicio3/Program.cs
@@ -8,8 +8,94 @@ namespace Ejercicio3
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            var violinista = new Violinista();
+            var clarinetista = new Clarinetista();
+
+            DirectorDeOrquesta director = new DirectorDeOrquesta(violinista, clarinetista);
+
+            director.EmpezarSinfonia();
+            director.TerminarSinfonia();
+
+            Console.ReadKey();
+        }
+    }
+
+    public abstract class Musico
+    {
+        public string Instrumento { get; private set; }
+
+        public Musico(string instrumento)
+        {
+            Instrumento = instrumento;
+        }
+
+        public abstract void Afinar();
+
+        public void Tocar()
+        {
+            Console.WriteLine(this.ToString() + " empieza a tocar");
+        }
+
+        public void Terminar()
+        {
+            Console.WriteLine(this.ToString() + " termina de tocar");
+        }
+    }
+
+    public class DirectorDeOrquesta
+    {
+        private Musico[] musicos;
+        public DirectorDeOrquesta(params Musico[] musicos)
+        {
+            this.musicos = musicos;
+
+            musicos[0].Afinar();
+            musicos[1].Afinar();
+        }
+
+        public void EmpezarSinfonia()
+        {
+            musicos[0].Tocar();
+            musicos[1].Tocar();
+        }
+
+        public void TerminarSinfonia()
+        {
+            musicos[0].Terminar();
+            musicos[1].Terminar();
+        }
+
+
+    }
+
+    public class Violinista : Musico
+    {
+        public Violinista()
+            : base("Violin")
+        {
+
+        }
+
+        public override void Afinar()
+        {
+            Console.WriteLine("El violinista está afinando");
+        }
+    }
+
+    public class Clarinetista : Musico
+    {
+        public Clarinetista()
+            : base("Clarinete")
+        {
+
+        }
+
+        public override void Afinar()
+        {
+            Console.WriteLine("El clarinetista está afinando");
         }
     }
 }
